196894495c91da1c7e2874c51469ae2439568b47
asd123745 <63216350+asd123745@users.noreply.github.com>
2020-06-19 11:25:56 +0900

    6/19

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRigidbody;  //이동에 사용할 리지드바디 컴포넌트
-    public float speed = 8f;  //이동 속력
-
-    // Start is called before the first frame update
+    public float speed = 8f;  //이동 속력
     void Start()
     {
-        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //수평축과 수직축의 입력값을 감지하여 저장
+        //수평축과 수직축의 입력값을 감지하여 저장
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
         //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
